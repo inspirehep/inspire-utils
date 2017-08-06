ddcbca9750947c4d301e8e571a4a0c3357e39876
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2017-08-06 17:59:59 +0200

    global: first commit
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

---
+++ README.rst
@@ -0,0 +1,14 @@
+===============
+ INSPIRE-Utils
+===============
+
+.. image:: https://travis-ci.org/inspirehep/inspire-utils.svg?branch=master
+    :target: https://travis-ci.org/inspirehep/inspire-utils
+
+.. image:: https://coveralls.io/repos/github/inspirehep/inspire-utils/badge.svg?branch=master
+    :target: https://coveralls.io/github/inspirehep/inspire-utils?branch=master
+
+About
+=====
+
+INSPIRE-specific utils.
---
+++ setup.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2014-2017 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+"""INSPIRE-specific utils."""
+
+from __future__ import absolute_import, division, print_function
+
+from setuptools import find_packages, setup
+
+
+URL = 'https://github.com/inspirehep/inspire-utils'
+
+setup_requires = [
+    'autosemver~=0.0,>=0.5.1',
+]
+
+install_requires = []
+
+docs_require = []
+
+tests_require = [
+    'pytest~=3.0,>=3.2.0',
+    'pytest-cov~=2.0,>=2.5.1',
+    'pytest-flake8~=0.0,>=0.8.1',
+]
+
+extras_require = {
+    'docs': docs_require,
+    'tests': tests_require,
+}
+
+extras_require['all'] = []
+for name, reqs in extras_require.items():
+    extras_require['all'].extend(reqs)
+
+packages = find_packages(exclude=['docs'])
+
+setup(
+    name='inspire-utils',
+    autosemver={
+        'bugtracker_url': URL + '/issues',
+    },
+    url=URL,
+    license='GPLv3',
+    author='CERN',
+    author_email='admin@inspirehep.net',
+    packages=packages,
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 4 - Beta',
+    ],
+)
