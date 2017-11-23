4fe172b62ec5a0fde7c73a08202bdbda6039e000
Chris Aslanoglou <chris.aslanoglou@gmail.com>
2017-11-23 14:35:05 +0100

    name: make firstname last with one name part input
    With only one name part as input, HumanName assigns it at one of its
    first, middle and suffix fields. This is not useful for our use-case
    since users will type lastnames if they insert only one name part.
    
    Signed-off-by: Chris Aslanoglou <chris.aslanoglou@gmail.com>

--- inspire_utils/name.py
+++ inspire_utils/name.py
@@ -67,7 +67,7 @@ class ParsedName(object):
         """Create a ParsedName instance.
 
         Args:
-            name (str): The name to be parsed (must be non empty nor None).
+            name (six.text_type): The name to be parsed (must be non empty nor None).
             constants (:class:`nameparser.config.Constants`): Configuration for `HumanName` instantiation.
                 (Can be None, if provided it overwrites the default one generated in
                 :method:`prepare_nameparser_constants`.)
@@ -76,6 +76,12 @@ class ParsedName(object):
             constants = ParsedName.constants
 
         self._parsed_name = HumanName(name, constants=constants)
+
+        # In the case of one name part, i.e. only lastname, name parser adds the name part in one of the first, middle,
+        # suffix fields (depending on input). For our use-case if a user types a name, usually would be a lastname.
+        if len(name.split()) == 1:
+            self._parsed_name.last = self._parsed_name.first + self._parsed_name.middle + self._parsed_name.suffix
+            self._parsed_name.first = ''
 
     def __len__(self):
         return len(self._parsed_name)
