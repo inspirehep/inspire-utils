4010122a6d92146bfd742ce36660f7c88a774556
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2017-12-22 09:18:09 +0100

    setup: reorder test dependencies
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- setup.py
+++ setup.py
@@ -47,11 +47,11 @@ install_requires = [
 docs_require = []
 
 tests_require = [
-    'mock~=2.0,>=2.0.0',
     'flake8-future-import~=0.0,>=0.4.3',
-    'pytest~=3.0,>=3.2.0',
+    'mock~=2.0,>=2.0.0',
     'pytest-cov~=2.0,>=2.5.1',
     'pytest-flake8~=0.0,>=0.8.1',
+    'pytest~=3.0,>=3.2.0',
 ]
 
 extras_require = {
