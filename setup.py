0e0784f29a750bd976f3f581244369f78ba2ba8b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-01-07 11:34:26 +0100

    setup: bump Unidecode
    Sem-ver: breaks compatibility
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- setup.py
+++ setup.py
@@ -36,12 +36,12 @@ setup_requires = [
 ]
 
 install_requires = [
+    'Unidecode~=1.0,>=1.0.22',
     'babel~=2.0,>=2.5.1',
     'lxml~=4.0,>=4.0.0',
     'nameparser~=0.0,>=0.5.3',
     'python-dateutil~=2.0,>=2.6.1',
     'six~=1.0,>=1.10.0',
-    'unidecode~=0.0,>=0.4.21',
 ]
 
 docs_require = []
