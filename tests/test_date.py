77a7b2b29601abf4588936bdc85d07cbe1f031e8
hanadikaleel <hanadikaleel@gmail.com>
2021-05-28 18:20:51 +0200

    updated tests

--- tests/test_date.py
+++ tests/test_date.py
@@ -37,6 +37,7 @@ def test_loads_validate_dates_day():
     with pytest.raises(ValueError):
         PartialDate.loads('2015-10-1')
 
+
 def test_loads_validate_dates_month():
     with pytest.raises(ValueError):
         PartialDate.loads('2015-1-10')
