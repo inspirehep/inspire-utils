135b4d5ad2c95541b0904bd29b0bd77b87fa4f35
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-05-06 13:16:35 +0200

    isbn: add normalize for isbn  * ref https://github.com/cern-sis/issues-inspire/issues/902

---
+++ inspire_utils/isbn.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2025 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+"""ISBN related utils"""
+
+import isbnlib
+
+def normalize_isbn(isbn):
+    """Normalize an ISBN to ISBN-13 format using isbnlib. If invalid, return as-is."""
+    try:
+        normalized = isbnlib.canonical(isbn)
+        
+        if isbnlib.is_isbn10(normalized):
+            return isbnlib.to_isbn13(normalized)
+        if isbnlib.is_isbn13(normalized):
+            return normalized
+    except Exception:
+        pass
+
+    return isbn
---
+++ tests/test_isbn.py
@@ -0,0 +1,62 @@
+import pytest
+import isbnlib
+from inspire_utils.isbn import normalize_isbn
+
+isbn_ok = {
+    '012345672X': '9780123456724',
+    '9780387308869': '9780387308869',
+    '9780393334777': '9780393334777',
+    '9781593273880': '9781593273880',
+    '9788478447749': '9788478447749'
+}
+
+isbn_nok = [
+    'X123456781',
+    '012345678X',
+    '9780123456780',
+    '9780123456781',
+    '9790123456780',
+    '9790123456781',
+    '9890123456781',
+    '',
+]
+
+
+@pytest.mark.parametrize("isbn_input,expected", list(isbn_ok.items()))
+def test_normalize_valid(isbn_input, expected):
+    normalized = normalize_isbn(isbn_input)
+    assert normalized == expected
+    assert isbnlib.is_isbn13(normalized)
+
+
+@pytest.mark.parametrize("isbn_input", isbn_nok)
+def test_normalize_invalid(isbn_input):
+    normalized = normalize_isbn(isbn_input)
+    assert normalized == isbn_input
+
+
+def test_normalize_with_hyphens():
+    isbn_with_hyphens = "978-0-387-30886-9"
+    expected = "9780387308869"
+    normalized = normalize_isbn(isbn_with_hyphens)
+    assert normalized == expected
+
+
+def test_normalize_with_spaces():
+    isbn_with_spaces = "978 0 393 33477 7"
+    expected = "9780393334777"
+    normalized = normalize_isbn(isbn_with_spaces)
+    assert normalized == expected
+
+
+def test_normalize_lowercase():
+    isbn_lowercase = "012345672x"
+    expected = "9780123456724"
+    normalized = normalize_isbn(isbn_lowercase)
+    assert normalized == expected
+
+
+def test_normalize_empty_string():
+    isbn_empty = ""
+    normalized = normalize_isbn(isbn_empty)
+    assert normalized == isbn_empty
